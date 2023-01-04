3de99602a9ffdf8c3d4e2064ab33d4353c5e5916
Tanzim Shahriar <tanzimshahriarfahim@gmail.com>
2023-01-04 18:42:10 +1100

    navbar closes on clicking link on mobile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,49 +9,58 @@ export const Navbar = () => {
                 Pipedrive Masters
             </div>
             <div
-                className={`items-center fixed h-full top-14 shadow-lg lg:shadow-none bg-white w-72 lg:w-auto lg:static lg:flex transform duration-500 ${
+                className={`items-center fixed h-full top-14 shadow-lg lg:shadow-none bg-white w-72 lg:w-auto lg:static lg:flex lg:flex-row-reverse transform duration-500 ${
                     isOpen ? "block opacity-100 right-0" : "-right-72"
                 }`}
             >
-                <Link
-                    className="block p-4 text-sm text-gray-400 hover:text-violet hover:underline transform duration-500"
-                    href="/"
-                >
-                    Unser Service
-                </Link>
-                <Link
-                    className="block p-4 text-sm text-gray-400 hover:text-violet hover:underline transform duration-500"
-                    href="/"
-                >
-                    Ablauf
-                </Link>
-                <Link
-                    className="block p-4 text-sm text-gray-400 hover:text-violet hover:underline transform duration-500"
-                    href="/"
-                >
-                    Case Studies
-                </Link>
-                <Link
-                    className="block p-4 text-sm text-gray-400 hover:text-violet hover:underline transform duration-500"
-                    href="/"
-                >
-                    Pricing
-                </Link>
-                <Link
-                    className="block p-4 text-sm text-gray-400 hover:text-violet hover:underline transform duration-500"
-                    href="/"
-                >
-                    FAQs
-                </Link>
+                <div className="p-4 pb-2 lg:p-0">
+                    <Link
+                        className="block bg-violet p-3 w-52 lg:w-auto text-sm lg:text-base lg:p-4 rounded-lg text-white"
+                        href="/"
+                        onClick={() => setIsOpen(!isOpen)}
+                    >
+                        Gespräch vereinbaren
+                    </Link>
+                </div>
+                <div className="flex flex-col lg:flex-row">
+                    <Link
+                        className="block p-4 text-sm text-gray-400 hover:text-violet hover:underline transform duration-500"
+                        href="/"
+                        onClick={() => setIsOpen(!isOpen)}
+                    >
+                        Unser Service
+                    </Link>
+                    <Link
+                        className="block p-4 text-sm text-gray-400 hover:text-violet hover:underline transform duration-500"
+                        href="/"
+                        onClick={() => setIsOpen(!isOpen)}
+                    >
+                        Ablauf
+                    </Link>
+                    <Link
+                        className="block p-4 text-sm text-gray-400 hover:text-violet hover:underline transform duration-500"
+                        href="/"
+                        onClick={() => setIsOpen(!isOpen)}
+                    >
+                        Case Studies
+                    </Link>
+                    <Link
+                        className="block p-4 text-sm text-gray-400 hover:text-violet hover:underline transform duration-500"
+                        href="/"
+                        onClick={() => setIsOpen(!isOpen)}
+                    >
+                        Pricing
+                    </Link>
+                    <Link
+                        className="block p-4 text-sm text-gray-400 hover:text-violet hover:underline transform duration-500"
+                        href="/"
+                        onClick={() => setIsOpen(!isOpen)}
+                    >
+                        FAQs
+                    </Link>
+                </div>
             </div>
-            <div className="hidden lg:block">
-                <Link
-                    className="block bg-violet p-4 rounded-lg text-white"
-                    href="/"
-                >
-                    Gespräch vereinbaren
-                </Link>
-            </div>
+
             <button
                 className="lg:hidden flex flex-col justify-center gap-2 w-min"
                 onClick={() => setIsOpen(!isOpen)}
